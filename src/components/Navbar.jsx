1c19d3e161dbe0f4455b2baba709a0eef636a0fa
David Poslušný <david.poslusny@email.cz>
2023-02-18 15:27:27 +0100

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { close, logo, menu } from '../assets'
+import { navLinks } from '../constants'
 
 const Navbar = () => {
+
+  const [toggle, setToggle] = useState(false)
+
   return (
-    <div>
-      Navbar  
-    </div>
+    <nav className='w-full flex py-6 justify-between items-center navbar'>
+      <img src={logo} alt='hoobank' className='w-[124px] h-[32px]'/>
+      <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
+        {navLinks.map((nav, i) => (
+          <li 
+            key={nav.id}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${i === navLinks.length - 1 ? 'mr-0' : 'mr-10'} text-white mr-10`}
+          >
+            <a href={`#${nav.id}`}>
+              {nav.title}
+            </a>
+          </li>        
+        ))}
+      </ul>
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <img
+          src={toggle ? close : menu}
+          alt='menu'
+          className='w-[28px] h-[28px] object-contain'
+          onClick={() => setToggle((previous) => !previous)}
+        />
+        <div className={`${toggle ? 'flex' : 'hidden'} p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+          <ul className='list-none flex flex-col justify-end items-center flex-1'>
+            {navLinks.map((nav, i) => (
+              <li 
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${i === navLinks.length - 1 ? 'mr-0' : 'mb-4'} text-white mr-10`}
+              >
+                <a href={`#${nav.id}`}>
+                  {nav.title}
+                </a>
+              </li>        
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
   )
 }
 
