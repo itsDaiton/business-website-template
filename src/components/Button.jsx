1cf33a24bd332244283018132e856ad8ffe3893d
David Poslušný <david.poslusny@email.cz>
2023-02-20 15:54:17 +0100

    business section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Button = () => {
+const Button = ({ styles }) => {
   return (
-    <div>
-      Button   
-    </div>
+    <button type='button' className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none ${styles}`}>
+      Get Started
+    </button>
   )
 }
 
