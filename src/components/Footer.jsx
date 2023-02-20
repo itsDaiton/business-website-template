abdac3b864189306e595fd545f7194961d97860c
David Poslušný <david.poslusny@email.cz>
2023-02-20 19:23:54 +0100

    bugfix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => {
         </div>
         <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
           {footerLinks.map((link) => (
-            <div key={link.key} className='flex flex-col ss:my-0 my-4 min-w-[150px]'>
+            <div key={link.title} className='flex flex-col ss:my-0 my-4 min-w-[150px]'>
               <h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white'>
                 {link.title}
               </h4>
