aae75979dde5b75b6077361d610d50f1e71cbb5c
David Poslušný <david.poslusny@email.cz>
2023-02-20 18:55:55 +0100

    call to action section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import styles from '../style'
+import Button from './Button'
 
 const CTA = () => {
   return (
-    <div>
-      CTA     
-    </div>
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className='flex-1 flex flex-col'>
+        <h2 className={styles.heading2}>Let's try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Everything you need to accept card payments and grow your business anywhere on the planet.
+        </p>
+      </div>
+      <div className={`${styles.flexCenter} sm:ml-10 ml-0`}>
+        <Button/>
+      </div>
+    </section>
   )
 }
 
