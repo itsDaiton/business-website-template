f94151691fa107b08b21cf47855e42f36491851d
David Poslušný <david.poslusny@email.cz>
2023-02-20 19:17:49 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,58 @@
 import React from 'react'
+import styles from '../style'
+import { logo } from '../assets'
+import { footerLinks, socialMedia } from '../constants'
 
 const Footer = () => {
   return (
-    <div>
-      Footer   
-    </div>
+    <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
+      <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
+        <div className='flex-1 flex flex-col justify-start mr-10'>
+          <img
+            src={logo}
+            alt='logo'
+            className='w-[266px] h-[172px] object-contain'
+          />
+          <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
+            A new way to make the payments easy, reliable and secure.         
+          </p>
+        </div>
+        <div className='flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10'>
+          {footerLinks.map((link) => (
+            <div key={link.key} className='flex flex-col ss:my-0 my-4 min-w-[150px]'>
+              <h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white'>
+                {link.title}
+              </h4>
+              <ul className='list-none mt-4'>
+                {link.links.map((item, index) => (
+                  <li 
+                    key={item.name} 
+                    className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== link.links.length - 1 ? 'mb-4' : 'mb-0'}`}
+                  >
+                    {item.name}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]'>
+        <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
+          2023 HooBank. All Rights Reserved.
+        </p>  
+        <div className='flex flex-row md:mt-0 mt-6'>
+          {socialMedia.map((social, index) => (
+            <img
+              src={social.icon}
+              key={social.id}
+              alt={social.id}
+              className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length - 1 ? 'mr-6' : 'mr-0'}`}
+            />
+          ))}
+        </div>    
+      </div>
+    </section>
   )
 }
 
