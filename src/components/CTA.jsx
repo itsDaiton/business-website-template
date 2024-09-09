8983b456530b58b5df210655385cd74ba857508a
Miguel Marcelino <mac.marcelino@outlook.pt>
2024-09-09 21:49:31 +0100

    Make text more appealing

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -7,7 +7,7 @@ const CTA = () => {
       className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px]`}
     >
       <div className="flex-1 flex flex-col">
-        <h2 className={styles.heading2}>Are you ready to print?</h2>
+        <h2 className={styles.heading2}>Are you ready to create?</h2>
         <p className={`${styles.paragraph} mt-5`}>
         We'd love to hear from you! Reach out to us today and take advantage of an exclusive offer: enjoy a 20% discount on every third item you print with us. 
         It's our way of saying thank you for choosing us. We look forward to helping you bring your ideas to life!
