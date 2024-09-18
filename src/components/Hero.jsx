1ce43aec45cd10aa811ec695a033ab09c23dcdde
Miguel Marcelino <mac.marcelino@outlook.pt>
2024-09-18 20:35:15 +0100

    clean: Remove get started

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import styles from "../styles/style";
 import { discount, dragon } from "../assets";
-import GetStarted from "./GetStarted";
 
 const Hero = () => {
   return (
@@ -49,9 +48,6 @@ const Hero = () => {
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
-      </div>
-      <div className={`ss:hidden ${styles.flexCenter}`}>
-        <GetStarted />
       </div>
     </section>
   );
