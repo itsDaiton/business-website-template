74bb972e479365daa12abb86ef82ad9bcbf04678
Miguel Marcelino <mac.marcelino@outlook.pt>
2024-09-18 23:18:04 +0100

    clean: Fix padding

--- src/styles/style.js
+++ src/styles/style.js
@@ -1,5 +1,5 @@
 const styles = {
-  boxWidth: "xl:max-w-[1280px] w-full",
+  boxWidth: "xl:max-w-[1280px] w-full sm:px-8 px-4",
 
   heading2: "font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
   paragraph: "font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px]",
