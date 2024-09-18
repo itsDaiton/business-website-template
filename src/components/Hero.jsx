0d02bf86c25cdda524bfb62d1db8c198f9829a95
Miguel Marcelino <mac.marcelino@outlook.pt>
2024-09-18 23:44:53 +0100

    fix: Fix allignments

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
       className={`flex md:flex-row flex-col ${styles.paddingY}`}
     >
       <div
-        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
+        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 md:px-16 px-6`}
       >
         {/* Discounts per order */}
         <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
@@ -17,24 +17,22 @@ const Hero = () => {
             <span className="text-white">20%</span> Discount on every{" "}
             <span className="text-white"> 3rd</span> Order
           </p>
-        
-        {/* Main text */}
         </div>
+        {/* Main text */}
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            Bringing <br className="sm:block hidden" />
-            <span className="text-gradient">Ideas</span>
+            Bringing <br className="md:block hidden" />
+            <span className="text-gradient">Ideas</span>{" "}
+            <br className="md:block hidden" />
+            to <span className="text-gradient">Life</span>
           </h1>
         </div>
-        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100px] leading-[75px] w-full">
-          to <span className="text-gradient">Life</span>.
-        </h1>
-        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        <p className={`${styles.paragraph} max-w-[500px] mt-5`}>
           <i>
             "Great things are not done by impulse, but by a series of small
             things brought together."
           </i>
-          - Vincent Van Gogh
+          <br></br>- Vincent Van Gogh
         </p>
       </div>
       <div
