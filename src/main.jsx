59cf1b382b98cd291de0e481ffd3c1fdc85f2664
Miguel Marcelino <mac.marcelino@outlook.pt>
2024-09-18 22:27:08 +0100

    clean: Separate image css

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './styles/index.css'
 import './styles/tables.css'
 import './styles/discounts.css'
 import './styles/logo.css'
+import './styles/images.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
