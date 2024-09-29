1c98c18628bb37dfb3909771edd955c0592066e1
Miguel Marcelino <mac.marcelino@outlook.pt>
2024-09-29 17:22:22 +0100

    feature: New landing page design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,42 +1,33 @@
 import styles from "../styles/style";
-import { discount, dragon } from "../assets";
+import landing_page from "../assets/video/landing_page.mp4";
 
 const Hero = () => {
   return (
     <section
       id="home"
-      className={`flex md:flex-row flex-col ${styles.paddingY}`}
+      className={`flex`}
     >
+      <div className={`landing-wrapper`}>
+        <video src={landing_page} autoPlay muted loop className={`video-style`}></video>
+      </div>
       <div
-        className={`flex-1 ${styles.flexStart} flex-col xl:px-0 md:px-16 px-6`}
+        className={`landing-title md:px-40 px-10`}
       >
         {/* Main text */}
-        <div className="flex flex-row justify-between items-center w-full">
-          <h1 className="flex-1 font-poppins font-semibold ss:text-[90px] text-[55px] text-white ss:leading-[120.8px] leading-[75px]">
+        <div className="w-full">
+          <h1 className={`font-poppins font-semibold sm:text-[110px] ss:text-[90px] text-[60px] text-white sm:leading-[140px] sm:leading-[130px] leading-[80px]`}>
             Turning <br className="md:block hidden" />
             <span className="text-gradient">Ideas</span>{" "}
             <br className="md:block hidden" />
             Into <span className="text-gradient">Reality</span>
           </h1>
         </div>
-        <p className={`${styles.paragraph} md:max-w-[500px] mt-10`}>
+        <p className={`${styles.paragraph}  text-[20px] md:max-w-[500px] md:mt-10 mt-20`}>
           <i>
             "Great things are not done by impulse, but by a series of small things brought together."
           </i>
           <br></br>- Vincent Van Gogh
         </p>
-      </div>
-      <div
-        className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
-      >
-        <img
-          src={dragon}
-          alt="billing"
-          className="dragon-style w-[100%] h-[100%] relative z-[2]"
-        />
-        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
-        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
       </div>
     </section>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './styles/tables.css'
 import './styles/discounts.css'
 import './styles/logo.css'
 import './styles/images.css'
+import './styles/landing-page.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
